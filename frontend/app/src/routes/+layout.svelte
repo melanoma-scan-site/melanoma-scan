6e80d773f005972de2ab6771ed4486e9cec573f4
Jesper Nielsen <jeni.skp@edu.mercantec.dk>
2024-11-13 16:14:13 +0100

    Added shadcn, tailwind theme, lucide icons and imported the components into the project

--- frontend/app/src/routes/+layout.svelte
+++ frontend/app/src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+<div class="min-h-screen">
+	{@render children()}
+</div>
--- frontend/app/src/routes/+page.svelte
+++ frontend/app/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Sun, Moon } from 'lucide-svelte';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<main class="p-4">
+	<Button onclick={toggleMode} variant="outline" size="icon">
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+</main>
