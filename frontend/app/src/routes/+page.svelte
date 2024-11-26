c48644b5d4bc38a818b7bb58249a4b5577570785
Viktor Viskov <carrergt@gmail.com>
2024-11-26 11:06:09 +0100

    Fix endpoint

--- frontend/app/src/routes/+page.svelte
+++ frontend/app/src/routes/+page.svelte
@@ -51,7 +51,7 @@
 
 			xhr.onerror = () => reject(new Error('Upload failed'));
 
-			xhr.open('POST', 'https://melanoma-scan.site/check');
+			xhr.open('POST', 'https://melanoma-scan.site/api/check');
 			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 			xhr.send(`base64_file=${encodeURIComponent(base64)}`);
 		});
